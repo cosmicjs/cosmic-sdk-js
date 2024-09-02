2918c43f677c6536945e12090e0bf7c182b73f21
Tony Spiro <tspiro@tonyspiro.com>
2024-09-02 10:08:10 -0700

    edit: remove alt_text (another PR)

--- src/types/media.types.ts
+++ src/types/media.types.ts
@@ -3,7 +3,6 @@ import { GenericObject } from './generic.types';
 export type InsertMediaType = {
   media: any;
   folder?: string;
-  alt_text?: string;
   metadata?: GenericObject;
   trigger_webhook?: boolean;
 };
