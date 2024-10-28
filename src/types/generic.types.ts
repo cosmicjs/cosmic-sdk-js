9057418b4bc3831da79c3c590e886c1869cf6cb0
jazibsawar <jazibsawar@gmail.com>
2024-10-29 00:31:18 +0200

    remove options

--- src/types/generic.types.ts
+++ src/types/generic.types.ts
@@ -1,8 +1,3 @@
 export type GenericObject = Record<string, any>;
 export type NonEmptyObject<T extends Record<string, unknown>> =
   T extends Record<string, never> ? never : T;
-
-export type GenericOptionsType = {
-  slug_field?: boolean;
-  content_editor?: boolean;
-};
--- src/types/objectRevision.type.ts
+++ src/types/objectRevision.type.ts
@@ -1,10 +1,9 @@
-import { GenericObject, GenericOptionsType } from './generic.types';
+import { GenericObject } from './generic.types';
 
 export type InsertObjectRevisionType = {
   title?: string;
   slug?: string;
   content?: string;
-  options?: GenericOptionsType;
   publish_at?: number;
   unpublish_at?: number;
   metadata?: GenericObject;
